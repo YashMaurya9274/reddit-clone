a1b2a7fe7f160f6f7c2481d986719ca82ec0ad4f
YashMaurya9274 <yash.maurya537468@gmail.com>
2022-06-10 23:57:22 +0530

    bug fix 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
       <div className="flex">
         <Feed />
 
-        <div className="sticky top-16 mx-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+        <div className="sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
           <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
 
           <div>
